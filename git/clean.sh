016e662ff06253b6c57e1bb88bb9ca3a9e536acb
Daniel Breton <d.breton.suarez@gmail.com>
2020-11-19 19:20:13 +0100

    Adds clean, improves synch

--- git/clean.sh
+++ git/clean.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#@(#) Synchronizes two git folders of the same repo without pushing or commit
+#@(#) Cleans folder and 1-level of subfolders
 
 usage () {
 	cat <<HELP_USAGE
-	$0 -d dest [-args]
+	$0  [-args]
 	------------------------------------
-	-d    Destination folder
-	-s    Source folder. Defaults assumes actual folder.
-	-c    Only check (Dry run)
 	-h    Shows this help
+	-s    Source folder. Defaults assumes actual folder.
+	-l    Not clean subfolders
+	-d    Dry-run
 HELP_USAGE
 exit 1
 }
@@ -28,37 +28,30 @@ clean_local() {
 	fi
 }
 
-synch_local() {
-	if [ $dry_run -gt 0 ];
-	then
-		to_copy=$(git status -s | egrep "M|A|AM|??" | rev | cut -d" " -f1 | rev)
-		echo "Will copy $to_copy"
-	else
-		to_copy=$(git status -s | egrep "M|A|AM|??" | rev | cut -d" " -f1 | rev)
-		cp --parents $to_copy $2
-	fi
-}
-
 dry_run=0
-while getopts hs:d:c flag
+only_locals=0
+while getopts hs:dl flag
 do
 	case "${flag}" in
 		h) usage;;
+		l) only_locals=1;;
+		d) dry_run=1;;
 		s) source=${OPTARG};;
-		d) dest=${OPTARG};;
-		c) dry_run=1
 	esac
 done
-
-if [ -z $dest ];
-then
-	usage
-fi
 
 if [ -z $source ];
 then
 	source=$(pwd)
 fi
 
-cd $dest && clean_local
-cd $source && synch_local
+cd $source
+clean_local
+subs=$(ls -d */)
+echo $subs
+for sub in $subs
+do
+	cd $sub
+	clean_local
+	cd ..
+done
